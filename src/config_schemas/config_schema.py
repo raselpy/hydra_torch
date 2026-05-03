03946a9ab1ab10992573b44cbf715c617e311dc7
rasel <rasel.py@gmail.com>
2026-05-03 06:33:38 +0200

    clean yaml

--- src/config_schemas/config_schema.py
+++ src/config_schemas/config_schema.py
@@ -2,18 +2,16 @@ from hydra.core.config_store import ConfigStore
 from pydantic.dataclasses import dataclass
 from omegaconf import MISSING
 
-from src.config_schemas import model_schema, data_module_schema
+from src.config_schemas import  data_module_schema
 
 
 @dataclass
 class Config:
     data:  data_module_schema.DataModuleConfig = MISSING
-    model: model_schema.ModelConfig = MISSING
 
 
 def setup_config() -> None:
     data_module_schema.setup_config()
-    model_schema.setup_config()
 
     cs = ConfigStore.instance()
     cs.store(name="config_schema", node=Config)
